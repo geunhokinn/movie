329f293990cb42ac6888e33618e0726827d2a1e8
geunhokinn <gghh1220@gmail.com>
2024-11-21 15:51:37 +0900

    Feat: Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,41 @@
-function Detail(){
-    return <h1>Detail</h1>
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Detail() {
+    const { id } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState(null);
+
+    const getMovie = useCallback(async () => {
+
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+        setLoading(false);
+    }, [id]);
+
+    useEffect(() => {
+        getMovie();
+    }, [getMovie]);
+
+    return (
+        <div>
+            {loading ? <h1>Loading...</h1> :
+                <div>
+                    <h1>{movie.title}</h1>
+                    <img src={movie.medium_cover_image} alt={movie.title} />
+                    <p>{movie.description_full}</p>
+                    <ul>
+                        {movie.genres.map((genre) => (
+                            <li key={genre}>{genre}</li>
+                        ))}
+                    </ul>
+                    <h3>Rating: {movie.rating}</h3>
+                    <h3>Runtime: {movie.runtime} minutes</h3>
+                </div>}
+        </div>
+    )
 }
 
 export default Detail;
